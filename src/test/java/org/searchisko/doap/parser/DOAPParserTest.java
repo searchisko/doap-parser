991e490996166cd4a96c817afb05ae279c50e677
Lukas Vlcek <lukas.vlcek@gmail.com>
2014-09-25 15:58:56 +0200

    Add tests using jsonassert.

--- src/test/java/org/searchisko/doap/parser/DOAPParserTest.java
+++ src/test/java/org/searchisko/doap/parser/DOAPParserTest.java
@@ -6,58 +6,62 @@
 
 package org.searchisko.doap.parser;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-import org.openrdf.repository.RepositoryException;
 import org.searchisko.doap.json.Converter;
 import org.searchisko.doap.model.Person;
 import org.searchisko.doap.model.Project;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.StringWriter;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 /**
  * @author Lukas Vlcek (lvlcek@redhat.com)
  */
 public class DOAPParserTest {
 
-	private static Logger log = LoggerFactory.getLogger(DOAPParserTest.class);
-
-	@Test
-	public void testProjectParsing() throws URISyntaxException {
-		try {
-			String path = getClass().getResource( "/doap-examples/doap_maven.rdf" ).getPath();
-			Project project = DOAPParser.deserializeProjectFromRDFFile(path);
-			try {
-				String json = Converter.objectToJSON(project);
-			} catch (IOException e) {
-				log.error("Error converting project to JSON", e);
-			}
-		} catch (RepositoryException e) {
-			fail(e.getMessage());
-		}
+	/**
+	 * Read file from classpath into String. UTF-8 encoding expected.
+	 *
+	 * @param filePath in classpath to read data from.
+	 * @return file content.
+	 * @throws IOException
+	 */
+	public String readStringFromClasspathFile(String filePath) throws IOException {
+		StringWriter stringWriter = new StringWriter();
+		IOUtils.copy(getClass().getResourceAsStream(filePath), stringWriter, "UTF-8");
+		return stringWriter.toString();
 	}
 
+	/**
+	 * Test parsing of project
+	 * @throws Exception
+	 */
 	@Test
-	public void testPersonParsing() throws URISyntaxException {
-		try {
-			String path = getClass().getResource( "/doap-examples/doap_maven.rdf" ).getPath();
-			Collection<Person> persons = DOAPParser.deserializePersonFromRDFFile(path);
-			assertEquals(14, persons.size());
-			try {
-				for (Person p : persons) {
-					String json = Converter.objectToJSON(p);
-				}
-			} catch (IOException e) {
-				log.error("Error converting person to JSON", e);
-			}
-		} catch (RepositoryException e) {
-			fail(e.getMessage());
+	public void testProjectParsing() throws Exception {
+		String path = getClass().getResource( "/doap-examples/doap_maven.rdf" ).getPath();
+		Project project = DOAPParser.deserializeProjectFromRDFFile(path);
+		String json = Converter.objectToJSON(project);
+		JSONAssert.assertEquals(readStringFromClasspathFile("/doap-json/maven.json"), json, JSONCompareMode.NON_EXTENSIBLE);
+	}
+
+	/**
+	 * Test parsing of person objects fro project
+	 * @throws Exception
+	 */
+	@Test
+	public void testPersonParsing() throws Exception {
+		String path = getClass().getResource( "/doap-examples/doap_maven.rdf" ).getPath();
+		Collection<Person> persons = DOAPParser.deserializePersonFromRDFFile(path);
+		assertEquals(14, persons.size());
+		// try to convert each person
+		for (Person p : persons) {
+			String json = Converter.objectToJSON(p);
 		}
 	}
 }
